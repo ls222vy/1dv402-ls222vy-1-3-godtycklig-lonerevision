bc681e41c1991ed719e26dc4858c366b87f3ee24
Lotiz <ls222vy@student.lnu.se>
2014-09-24 21:52:52 +0200

    still working on, its not final

--- SalaryRevision/SalaryRevision/Program.cs
+++ SalaryRevision/SalaryRevision/Program.cs
@@ -10,7 +10,6 @@ namespace SalaryRevision
     {
         static void Main(string[] args)
         {
-
             int noOfSalaries = 0;
            
             while (true)
@@ -28,13 +27,16 @@ namespace SalaryRevision
                     Console.ResetColor();
                 } 
             }
+
             ProcessSalaries(noOfSalaries);
+            
             {
                 Console.WriteLine();
-
+               
                 Console.BackgroundColor = ConsoleColor.Green;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Tryck tangent för ny beräkning - Esc avslutar,");
+                
                 Console.ResetColor();
 
                 ConsoleKeyInfo cki;
@@ -46,33 +48,70 @@ namespace SalaryRevision
             }
         
         }
-        static void ProcessSalaries(int count) 
+        static void ProcessSalaries(int count)
         {
+            
+            int MedianSalary;
+             int SalarySpread;
+            double AverageSalary;
             int [] Salaries = new int  [count];
             int [] SalariesLength = new int [count];
-
+            //int [] SalariesSorted;
+           
             Salaries = new int [count];
            
+            // Läs i lönerna från användaren -------------------------------------------------------------------------
             for (int i = 0; i < count; i++) 
             { 
-            Salaries[i] =ReadInt(string.Format("Ange lön nummer {0}:", i+ 1));
-
+                Salaries[i] = ReadInt(string.Format("Ange lön nummer {0}: ", i+ 1));
              }
-                   Console.Write("\n..................................");
+                   Console.WriteLine("..................................");
                    Array.Copy(Salaries,SalariesLength,count);
                    Array.Sort(Salaries);
            
-            if (count %2 == 0) 
+            // Medianlön
+            // Avgör om det är ett jämnt antal löner -------------------
+            if (count % 2 == 0) 
              {
-                int medianFirst = Salaries[Salaries.Length /2];
+                // Jämnt antal löner. Slå ihop de två mittersta lönerna och dela med 2
+                 MedianSalary = (Salaries[(count / 2)-1] + Salaries[(count / 2)]) / 2;
              }
+            else 
+            {
+                // Udda antal löner
+                MedianSalary = Salaries[(count-1) / 2];
+            }
+            Console.WriteLine("Medianlön :        {0:c0}", MedianSalary );
 
-          }
+            // Medellön
+            AverageSalary = Salaries.Average();
+            Console.WriteLine("Medellön :         {0:c0}", AverageSalary);
+
+            // Lönespridning
+            SalarySpread = Salaries.Max() - Salaries.Min();
+            Console.WriteLine("Lönespridning :    {0:c0}", SalarySpread);
+            Console.Write("...................................");
+            Console.WriteLine();
+            // Skriv ut lönerna i den ordning de matats in
+            
+            for  (int i = 1; i<= count;i++)
+            {
+                Console.Write(" {0, 5} " , Salaries[i-1]);
+               
+                if (i % 3 == 0) 
+                {
+                Console.WriteLine();
+                }
+
+            }
+
+
+
+        }  // End ProcessSalaries Method
         
         static int ReadInt(string prompt)
         {
             string tempvar = null;
-
             while(true)
             {
                 try
@@ -80,8 +119,6 @@ namespace SalaryRevision
                     Console.Write(prompt);
                     tempvar = Console.ReadLine();
                     return int.Parse(tempvar);
-
-                  
                 }
                 catch
                 {
@@ -90,9 +127,8 @@ namespace SalaryRevision
                     Console.ResetColor();
                 }
             }
-
-            //return ReadInt;
         }
+
     }
 }
    
