499dfde5a113bac0d1edc3303a6036b161eb33f6
unknown <ls222vy@NY107X04.lnu.se>
2014-09-25 11:24:41 +0200

    final and approved

--- SalaryRevision/SalaryRevision/Program.cs
+++ SalaryRevision/SalaryRevision/Program.cs
@@ -11,108 +11,94 @@ namespace SalaryRevision
         static void Main(string[] args)
         {
             int noOfSalaries = 0;
-           
-            while (true)
+            do
             {
+                Console.Clear();
+
                 noOfSalaries = ReadInt("Ange antal löner att mata in : ");
+                Console.WriteLine();
 
                 if (noOfSalaries >= 2)
                 {
-                    break;
+                    ProcessSalaries(noOfSalaries);
                 }
                 else
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.Write("Du måste mata in minst Två löner");
                     Console.ResetColor();
-                } 
-            }
+                }
 
-            ProcessSalaries(noOfSalaries);
-            
-            {
                 Console.WriteLine();
-               
                 Console.BackgroundColor = ConsoleColor.Green;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Tryck tangent för ny beräkning - Esc avslutar,");
-                
                 Console.ResetColor();
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
-                ConsoleKeyInfo cki;
-                cki = Console.ReadKey(true);
-                if (cki.Key == ConsoleKey.Escape) 
-                {
-                    return;
-                }
-            }
-        
         }
         static void ProcessSalaries(int count)
         {
-            
-            int MedianSalary;
-             int SalarySpread;
-            double AverageSalary;
-            int [] Salaries = new int  [count];
-            int [] SalariesLength = new int [count];
+            int medianSalary;
+            int salarySpread;
+            double averageSalary;
+            int[] salaries = new int[count];
+            int[] salariesLength = new int[count];
             //int [] SalariesSorted;
-           
-            Salaries = new int [count];
-           
+
+            salaries = new int[count];
+
             // Läs i lönerna från användaren -------------------------------------------------------------------------
-            for (int i = 0; i < count; i++) 
-            { 
-                Salaries[i] = ReadInt(string.Format("Ange lön nummer {0}: ", i+ 1));
-             }
-                   Console.WriteLine("..................................");
-                   Array.Copy(Salaries,SalariesLength,count);
-                   Array.Sort(Salaries);
-           
+            for (int i = 0; i < count; i++)
+            {
+                salaries[i] = ReadInt(string.Format("Ange lön nummer {0}: ", i + 1));
+            }
+            Console.WriteLine("..................................");
+            Array.Copy(salaries, salariesLength, count);
+            Array.Sort(salaries);
+
             // Medianlön
             // Avgör om det är ett jämnt antal löner -------------------
-            if (count % 2 == 0) 
-             {
+            if (count % 2 == 0)
+            {
                 // Jämnt antal löner. Slå ihop de två mittersta lönerna och dela med 2
-                 MedianSalary = (Salaries[(count / 2)-1] + Salaries[(count / 2)]) / 2;
-             }
-            else 
+                medianSalary = (salaries[(count / 2) - 1] + salaries[(count / 2)]) / 2;
+            }
+            else
             {
                 // Udda antal löner
-                MedianSalary = Salaries[(count-1) / 2];
+                medianSalary = salaries[(count - 1) / 2];
             }
-            Console.WriteLine("Medianlön :        {0:c0}", MedianSalary );
+            Console.WriteLine("Medianlön :        {0:c0}", medianSalary);
 
             // Medellön
-            AverageSalary = Salaries.Average();
-            Console.WriteLine("Medellön :         {0:c0}", AverageSalary);
+            averageSalary = salaries.Average();
+            Console.WriteLine("Medellön :         {0:c0}", averageSalary);
 
             // Lönespridning
-            SalarySpread = Salaries.Max() - Salaries.Min();
-            Console.WriteLine("Lönespridning :    {0:c0}", SalarySpread);
+            salarySpread = salaries.Max() - salaries.Min();
+            Console.WriteLine("Lönespridning :    {0:c0}", salarySpread);
             Console.Write("...................................");
             Console.WriteLine();
             // Skriv ut lönerna i den ordning de matats in
-            
-            for  (int i = 1; i<= count;i++)
-            {
-                Console.Write(" {0, 5} " , Salaries[i-1]);
-               
-                if (i % 3 == 0) 
-                {
-                Console.WriteLine();
-                }
 
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write(" {0, 5} ", salaries[i - 1]);
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
             }
 
-
-
         }  // End ProcessSalaries Method
-        
+
         static int ReadInt(string prompt)
         {
             string tempvar = null;
-            while(true)
+
+            while (true)
             {
                 try
                 {
@@ -128,7 +114,5 @@ namespace SalaryRevision
                 }
             }
         }
-
     }
 }
-   
